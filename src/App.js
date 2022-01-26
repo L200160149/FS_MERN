f650061a03c895e8e0a2a5244c93fe4d2161b5d0
L200160149 <ums.informatika@gmail.com>
2022-01-26 12:33:09 +0700

    header and arrange to Landing Page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import 'assets/scss/style.scss';
+import "assets/scss/style.scss";
+import LandingPage from 'pages/LandingPage';
 
 function App() {
-  return (
-    <div className="App">
-      hello world
+  return <div className="App">
+      <Router>
+        <Route path="/" component={LandingPage}></Route>
+      </Router>
     </div>
-  );
 }
 
 export default App;
