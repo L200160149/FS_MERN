e1a92e0231dd59294b173f312cb06ebb7df8d227
L200160149 <ums.informatika@gmail.com>
2022-01-27 16:20:19 +0700

    undone testimony

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,6 +4,7 @@ import Header from 'parts/Header';
 import Hero from 'parts/Hero';
 import MostPicked from 'parts/MostPicked';
 import Categories from 'parts/Categories';
+// import Testimony from 'parts/Testimony';
 
 import landingPage from 'json/landingPage.json';
 
@@ -25,6 +26,8 @@ export default class LandingPage extends Component {
       />
 
       <Categories data={landingPage.categories} />
+
+      {/* <Testimony data={landingPage.rate} /> */}
     </>
     );
   }
