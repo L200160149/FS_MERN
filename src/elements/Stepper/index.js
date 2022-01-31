ddc2ed0ca41a20aec3be6bc0862f3f9de8ce3914
L200160149 <ums.informatika@gmail.com>
2022-01-31 18:21:08 +0700

    undone checkout

---
+++ src/elements/Stepper/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// import proptypes jika Stepper menerima data props
+import PropTypes from 'prop-types';
+
+export default function Stepper(props) {
+  return <div></div>;
+}
